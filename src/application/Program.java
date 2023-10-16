168c82d0ef6f6eb63dea879d264e76b62a583d40
Massu <massu.queirantes@yahoo.com.br>
2023-10-15 23:44:47 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +14,10 @@ public class Program {
 		Department obj = new Department(1,"Books");
 		
 		Seller seller  = new Seller(21, "Bob","bob@gmail.com",new Date(),3000.00, obj );
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
 		System.out.println(seller);
 
 	}
